8923c3c636487ba02628a4184f3ea7d3396c825b
dogeja <disk6473@gmail.com>
2024-02-20 22:18:22 +0900

    git ignore test

--- js/weather.js
+++ js/weather.js
@@ -1,25 +1,23 @@
-const API_KEY = "0077c9a9065ab7ce4363da81d76f5703";
+import config from "./config.js";
+const { API_KEY } = config;
 
-function onGeoOk(position){
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
-    const url = 
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-    fetch(url)
-     .then(Response => Response.json())
-     .then(data =>{
-            const cityName = document.querySelector("#weather>span:first-child");
-            const weatherStatus = document.querySelector("#weather>span:last-child");
-            
-        cityName.innerText= data.name;
-        weatherStatus.innerText= `${data.weather[0].main}/${data.main.temp}`;
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((Response) => Response.json())
+    .then((data) => {
+      const cityName = document.querySelector("#weather>span:first-child");
+      const weatherStatus = document.querySelector("#weather>span:last-child");
 
+      cityName.innerText = data.name;
+      weatherStatus.innerText = `${data.weather[0].main}/${data.main.temp}`;
+      console.log(weatherStatus);
     });
 }
-function onGeoError(){
-    alert("Can't find you. No weather for you.")
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
 }
-
-
 
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
